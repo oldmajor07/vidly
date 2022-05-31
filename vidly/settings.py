cba16cec4a0717f267675e2f79c8562b733bbd35
Femi <fabiyi.fe@gmail.com>
2022-05-31 18:58:55 -0400

    Initial commit

--- vidly/settings.py
+++ vidly/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+import os
 import os.path
 from pathlib import Path
 
@@ -20,13 +21,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^7v!zvg5ldm=$_ql904kkb5dym@&()*xc__bay#3ftmh$ut_on'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-^7v!zvg5ldm=$_ql904kkb5dym@&()*xc__bay#3ftmh$ut_on')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
